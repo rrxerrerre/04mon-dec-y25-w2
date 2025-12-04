a151e51261dab05915504b35be3387e521ca5a56
Riccardo Randazzo <riccardorandazzo8@gmail.com>
2025-12-04 18:05:02 +0100

    function

--- assets/function.js
+++ assets/function.js
@@ -3,10 +3,17 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-const Area = function (width, height) {
+
+/*const Area = function (width, height) {
   return width * height;
 };
-console.log(Area(2, 8));
+console.log(Area(2, 8));*/
+
+function area(l1, l2) {
+  const result = l1 * l2;
+  console.log(area(3, 4));
+}
+area(3, 4);
 
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
@@ -15,7 +22,7 @@ console.log(Area(2, 8));
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-const crazySum = function (num1, num2) {
+/*function crazySum (num1, num2) {
   if (num1 === num2) {
     return (num1 + num2) * 3;
   } else {
@@ -23,21 +30,31 @@ const crazySum = function (num1, num2) {
   }
 };
 console.log(crazySum(8, 8));
-console.log(crazySum(7, 8));
+console.log(crazySum(7, 8));*/
+
+function crazySum(n1, n2) {
+  const sum = n1 + n2;
+  if (n1 === n2) {
+    return sum * 3;
+  } else {
+    return sum;
+  }
+}
+
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-const crazyDiff = function (i) {
+function crazyDiff(i) {
   const diff = Math.abs(i - 19);
   if (i > 19) {
     return diff * 3;
   } else {
     return diff;
   }
-};
+}
 console.log(crazyDiff(10));
 console.log(crazyDiff(25));
 
@@ -45,9 +62,13 @@ console.log(crazyDiff(25));
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
-const boundary = function (i) {
-  return (i >= 20 && i <= 100) || i === 400;
-};
+function boundary(i) {
+  if ((i >= 20 && i <= 100) || i === 400) {
+    return true;
+  } else {
+    return false;
+  }
+}
 console.log(boundary(20));
 console.log(boundary(100));
 console.log(boundary(400));
@@ -60,7 +81,15 @@ console.log(boundary(8));
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
  ritornare la stringa originale senza alterarla.
 */
-const epify = function (Epicode) {};
+function epify(str) {
+  if (str.toLowerCase().startsWith("epicode")) {
+    //string methos MDN - toLowerCase startWith sono metodi
+    return str;
+  } else {
+    return "EPICODE" + str;
+  }
+}
+console.log(epify("è un mondo bellissimo"));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -70,9 +99,9 @@ const epify = function (Epicode) {};
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-const check3and7 = function (i) {
+function check3and7(i) {
   return i % 3 === 0 || i % 7 === 0;
-};
+}
 console.log(check3and7(9));
 console.log(check3and7(28));
 /* ESERCIZIO 7
@@ -90,7 +119,24 @@ console.log(reverseString("Epicode"));
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* SCRIVI QUI LA TUA RISPOSTA */ /*da stringa ad array, un array vuole sempre un ciclo for loop - Charat*/
+function upperFirst(phrase) {
+  console.log(phrase);
+  const words = phrase.split("");
+  console.log(words);
+  const finalPhrase = [];
+  for (let i = 0; i < words, length; i++) {
+    const word = word[i];
+    console.log(word);
+    const firstLetter = word[0].toUpperCase();
+    console.log(firstLetter);
+    const upperWord = word.slice(1);
+    console.log(upperWord);
+  }
+  console.log(finalPhrase.join(" "));
+  return finalPhrase.join("");
+}
+console.log(upperFirst("questo weekend tocchiamo l'erba fuori"));
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
